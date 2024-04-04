f5a6a5e7fc9c0a1ae73c5f38f013ee194c9f4539
sungmin Hong <johnyhong24@gmail.com>
2024-04-04 17:37:09 +0900

    Fix: login, signup 깃이 혼동되어서 발생된 문제해결

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -5,6 +5,8 @@ import PostDetail from "../pages/posts/detail";
 import PostNew from "../pages/posts/new";
 import PostEdit from "../pages/posts/edit";
 import ProfilePage from "../pages/profile";
+import LoginPage from "../pages/login";
+import SignupPage from "../pages/signup";
 
 export default function Router() {
   return (
@@ -16,6 +18,8 @@ export default function Router() {
         <Route path="/posts/new" element={<PostNew />} />
         <Route path="/posts/edit/:id" element={<PostEdit />} />
         <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
     </>
