3586f2328b5bdf60cb20a21276bd561343daefa4
sungmin Hong <johnyhong24@gmail.com>
2024-04-05 09:52:51 +0900

    Feat: PostDetail 페이지의 기본구조와 CSS 스타일링 추가

---
+++ src/components/PostDetail.tsx
@@ -0,0 +1,36 @@
+export default function PostDetail() {
+  return (
+    <>
+      <main className="post__detail">
+        <article className="post__box">
+          <h1 className="post__title">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          </h1>
+          <section className="post__profile-box">
+            <div className="post__profile"></div>
+            <div className="post__author-name">테스트명</div>
+            <div className="post__date">2024.04.04 목요일</div>
+          </section>
+          <section className="post__utils-box">
+            <div className="post__delete">삭제</div>
+            <div className="post__edit">수정</div>
+          </section>
+          <p className="post__text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            ultrices nisi id leo bibendum commodo. Integer semper venenatis
+            congue. Sed sed nisl accumsan, eleifend elit vestibulum, maximus mi.
+            Nullam fermentum metus erat. Nulla non elementum lorem. In interdum
+            eu mi eget tristique. Fusce faucibus ultricies elit et gravida.
+            Vivamus scelerisque quam placerat, vulputate massa in, pulvinar
+            erat. Cras posuere ipsum at nunc suscipit, eu egestas mi mattis.
+            Fusce purus metus, ornare ac nisi sit amet, maximus consequat risus.
+            In porta volutpat velit quis gravida. Nulla dapibus sed ipsum et
+            feugiat. Curabitur posuere a orci ut viverra. Donec nec augue
+            auctor, blandit velit in, mattis mauris. Nullam egestas blandit
+            lorem, ut commodo nunc rutrum et.
+          </p>
+        </article>
+      </main>
+    </>
+  );
+}
